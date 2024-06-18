ecc3525f7773e953f69996625ac883f4b05ebd57
itllsendamsg <marcin.chudy.office@gmail.com>
2024-06-18 02:33:21 +0200

    18 6 24 / The beginning of the 100 Ideas project and the "README.md" file updated.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $pl.stormit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
